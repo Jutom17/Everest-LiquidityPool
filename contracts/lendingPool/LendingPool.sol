8e729371bb4ef0488a46cfdfefb2e45f37e2817b
pedrommaiaa <pedroceduca@gmail.com>
2021-10-12 17:13:15 -0400

    need to calculate interest rates

--- contracts/lendingPool/LendingPool.sol
+++ contracts/lendingPool/LendingPool.sol
@@ -16,11 +16,17 @@ import { SafeERC20 } from "../libraries/SafeERC20.sol";
 contract LendingPool is ILendingPool {
     using SafeERC20 for IERC20; 
    
-    // mapping of user address -> (asset addresses -> amount of respective asset) 
+    // mapping of user address -> (asset addresses -> amount deposited of respective asset) 
     mapping(address => mapping(address => uint256)) private UserDeposits;
     
-    // mapping for each asset address and its total amount in the pool
-    mapping(address => uint256) public TotalPoolAssets; 
+    // mapping for each asset address and its total amount deposited in the pool
+    mapping(address => uint256) public TotalAssetSupply;
+
+
+    // mapping of user address -> (asset addresses -> amount borrowed of respective asset) 
+    mapping(address => mapping(address => uint256)) private UserBorrows;
+    // mapping for each asset address and its total amount borrowed from the pool
+    mapping(address => uint256) public TotalAssetBorrowed;
     
 
     /**
@@ -32,10 +38,10 @@ contract LendingPool is ILendingPool {
         // ToDo - update user Rates, acummulated Interest
 
         // Updates User deposit balance
-        UserDeposits[msg.sender][_asset] = UserDeposits[msg.sender][_asset] + _amount;
+        UserDeposits[msg.sender][_asset] += _amount;
         
         // Update total assets deposited in the pool
-        TotalPoolAssets[_asset] = TotalPoolAssets[_asset] + _amount;
+        TotalAssetSupply[_asset] += _amount;
 
         emit Deposit(msg.sender, _asset, _amount);
     }
@@ -52,10 +58,10 @@ contract LendingPool is ILendingPool {
         require(UserDeposits[msg.sender][_asset] >= _amount)
         
         // Updates User deposit balance
-        UserDeposits[msg.sender][_asset] = UserDeposits[msg.sender][_asset] - _amount;
+        UserDeposits[msg.sender][_asset] -= _amount;
         
         // Subtract amount withdrawn from the total of the pool
-        TotalPoolAssets[_asset] = TotalPoolAssets[_asset] - _amount;
+        TotalAssetSupply[_asset] -= _amount;
 
         emit Withdraw(msg.sender, _asset, _amount);
     }
@@ -67,6 +73,14 @@ contract LendingPool is ILendingPool {
     * @ param amount - The amount to be borrowed.
     **/
     function borrow(address _asset, uint256 _amount) external {
+    
+        // Check if user has enough collateral to borrow
+
+        // Update amount borrowed by the user
+        UserBorrows[msg.sender][_asset] += _amount;
+        
+        // Update total borrowed of respective asset in the pool
+        TotalAssetBorrowed[_asset] += _amount;
         
         emit Borrow(msg.sender, _asset, _amount); 
     }
@@ -77,6 +91,14 @@ contract LendingPool is ILendingPool {
     * @param amount - The amount to repay.
     **/
     function repay(address _asset, uint256 _amount) external {
+        
+        // Check if amount being repayed is == amount borrowed + fees     
+
+        // Update amount borrowed by the user
+        UserBorrows[msg.sender][_asset] -= _amount;
+        
+        // Update total borrowed of respective asset in the pool
+        TotalAssetBorrowed[_asset] -= _amount;
 
         emit Repay(msg.sender, _asset, _amount);    
     }
