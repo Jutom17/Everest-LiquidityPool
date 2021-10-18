f8e6d2444ea397b72e7b0de3fadc4e069bb6dddf
pedrommaiaa <pedroceduca@gmail.com>
2021-10-18 18:29:13 -0400

    update

--- contracts/lendingPool/LendingPool.sol
+++ contracts/lendingPool/LendingPool.sol
@@ -1,69 +1,62 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
+pragma solidity ^0.8.0;
 
 import { ILendingPoolCore } from "./interfaces/ILendingPoolCore.sol";
 import { SafeMath } from "../libraries/SafeMath.sol";
 import { IERC20 } from "../libraries/IERC20.sol";
+import { ERC20 } from "../libraries/ERC20.sol";
 import { SafeERC20 } from "../libraries/SafeERC20.sol";
 
 
-contract LendingPool is ILendingPoolCore {
+contract LendingPool is ILendingPoolCore, ERC20 {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
         
-    IERC20 public eToken;
-    IERC20 public poolToken;
+    IERC20 public immutable poolToken;
 
-    struct Account {
-        uint256 balance; 
-        uint256 lastRewardReceived;
-    }
-    
+    // mapping of user address -> balance 
+    mapping(address => uint256) private _balances;
+    uint256 public _poolTokenSupply;
 
-    constructor(address _eToken, address _poolToken) public {
-        eToken = IERC20(_eToken);
+    constructor(
+        string memory name,
+        string memory symbol,
+        address _poolToken 
+    ) ERC20(name, symbol) { 
         poolToken = IERC20(_poolToken);
     }
     
-    // mapping of user address -> balance) 
-    mapping(address => Account) private _balances;
-    uint256 _totalSupply;
-    uint256 _totalRewards;
-
-
-    function deposit(address _token, uint256 _amount) external {
-        // Check which cosumes less gas on avalanche, if or require
-        require(_token == poolToken, "Not everest token");
-        require(_amount > 0, "Can't deposit 0")
+    function deposit(uint256 _amount) external {
+        require(_amount > 0, "Can't deposit 0");
 
         // Updates User balance
         _balances[msg.sender] = _balances[msg.sender].add(_amount);
         
         // Update total deposited in the pool
-        _totalSupply = _totalSupply.add(_amount);
+        _poolTokenSupply = _poolTokenSupply.add(_amount);
 
         poolToken.safeTransferFrom(msg.sender, address(this), _amount);
 
-        // TODO mint and give user corresponding eToken values.
-        // eTokens amount == _amount
-        
-        // eToken.safeTransfer(msg.sender, _amount);
+        // Mint and give user corresponding eToken values.
+        _mint(msg.sender, _amount);
+        //eToken.safeTransfer(msg.sender, _amount);
 
         emit Deposit(msg.sender, _amount);
     }
 
 
-    function withdraw(address _eToken, uint256 _amount) external { 
+    function withdraw(uint256 _amount) external { 
         // Check to see if user has enough funds, and amount > 0
-        require(_balances[msg.sender] >= _amount && _amount > 0)
+        require(_balances[msg.sender] >= _amount && _amount > 0);
           
         // Updates User balance
         _balances[msg.sender] = _balances[msg.sender].sub(_amount);  
-        _totalSupply = _totalSupply.sub(_amount);
+        _poolTokenSupply = _poolTokenSupply.sub(_amount);
 
-        // TODO burn eToken
-
+        // burn eToken
+        _burn(msg.sender, _amount);
         poolToken.safeTransfer(msg.sender, _amount);
+
         emit Withdraw(msg.sender, _amount);
     }
 
--- contracts/lendingPool/interfaces/ILendingPoolCore.sol
+++ contracts/lendingPool/interfaces/ILendingPoolCore.sol
@@ -1,34 +1,30 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
+pragma solidity ^0.8.0;
 
 
 interface ILendingPoolCore {
     /**
     * @dev Emitted on deposit()
-    * @param asset - The address of the underlying asset of the reserve
     * @param user - The address initiating the deposit
     * @param amount - The amount deposited
     **/
     event Deposit(
         address user,
-        address indexed asset,
         uint256 amount
     );
 
     /**
     * @dev Emitted on withdraw()
-    * @param asset - The address of the underlyng asset being withdrawn
     * @param user - The address initiating the withdrawal
     * @param amount - The amount to be withdrawn
     **/
     event Withdraw(
         address indexed user, 
-        address indexed asset, 
         uint256 amount
     );
     
-    function deposit(address _token, uint256 _amount) external {}
+    //function deposit(address _token, uint256 _amount) external {}
 
     
-    function withdraw(uint256 _amount) external {}
+    //function withdraw(uint256 _amount) external {}
 }
